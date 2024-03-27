80cc567d25c2f26120e66561f7d264de4ef3b960
klessiton <klessitom@gmail.com>
2024-03-26 23:08:21 -0300

    feat: migrate build to vitejs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+const noAttr = () => {
+  return {
+    name: 'no-attribute',
+    transformIndexHtml(html: string) {
+      return html.replace(`type="module" crossorigin`, 'defer="true');
+    },
+  };
+};
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
+  base: '',
 });
