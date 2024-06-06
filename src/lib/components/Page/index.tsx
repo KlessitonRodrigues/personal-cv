4acf014558becfb4b5e6f95d64de25f68e968168
klessiton <klessitom@gmail.com>
2024-06-06 07:56:37 -0300

    fix: scroll top on changing page

--- src/lib/components/Page/index.tsx
+++ src/lib/components/Page/index.tsx
@@ -1,9 +1,14 @@
-import { PropsWithChildren } from 'react';
+import { PropsWithChildren, useEffect } from 'react';
 
 import { PageContainer, PageContent } from 'src/lib/styled/Containers';
 
 const Page = (props: PropsWithChildren) => {
   const { children } = props;
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   return (
     <PageContainer>
       <PageContent>{children}</PageContent>
