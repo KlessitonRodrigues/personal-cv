78f75d71b8b2a8ae26bbe6243c5f510eaff70edc
klessiton <klessitom@gmail.com>
2024-06-20 11:30:44 -0300

    chore: update website tests

--- _tests/src/tests/navigation/pages.spec.ts
+++ _tests/src/tests/navigation/pages.spec.ts
@@ -4,21 +4,17 @@ import { environment } from '../../config/env';
 import webDriver from '../../config/webDriver';
 import { scrollPage } from '../../utils/browserTest';
 
-describe('When on mobile screen', () => {
-  test('Should scroll pages and use mobile navigation bar', async () => {
+describe('When on desktop screen', () => {
+  test('Should scroll pages and use navigation bar', async () => {
     const browser = await webDriver;
     try {
       await browser.get(environment.WEBSITEURL);
-      await browser.manage().window().setRect({ width: 450, height: 1080 });
+      await browser.sleep(500);
       await browser.executeScript(scrollPage(10000));
       await browser.sleep(500);
       await browser.executeScript(scrollPage(0));
       await browser.sleep(500);
-      await browser.findElement(By.css('.icon-menu')).click();
-      await browser.sleep(500);
       await browser.findElement(By.css('a[href="/resume"]')).click();
-      await browser.sleep(500);
-      await browser.findElement(By.css('.icon-close')).click();
       await browser.sleep(500);
       await browser.executeScript(scrollPage(10000));
       await browser.sleep(500);
