be1b5598244c96d7f901f5e4c3a04b38d375cb2f
klessiton <klessitom@gmail.com>
2024-08-20 09:24:25 -0300

    feat: add tailwind colors

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -3,9 +3,9 @@ import { antColors as ac, materialColors as mc } from 'src/styles/colors';
 export const themeColors: Record<string, { main: string; mainBg: string }> = {
   indigo: { main: mc.indigo['400'], mainBg: mc.indigo['800'] },
   purple: { main: mc.purple['400'], mainBg: mc.purple['800'] },
-  blue: { main: ac.blue['500'], mainBg: ac.blue['700'] },
+  blue: { main: ac.blue['600'], mainBg: ac.blue['800'] },
   magenta: { main: ac.magenta['700'], mainBg: ac.magenta['900'] },
-  green: { main: ac.green['700'], mainBg: ac.green['800'] },
+  green: { main: ac.green['700'], mainBg: ac.green['900'] },
 };
 
 export const getColor = () => {
