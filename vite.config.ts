3ad2b7391ecb1429943b4e58247fead83bd3fbc9
klessiton <klessitom@gmail.com>
2025-04-04 07:10:26 -0300

    feat: add image optimizer

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
+import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
+const imagePlugin = ViteImageOptimizer({
+  jpg: { quality: 20 },
+  png: { quality: 20 },
+  jpeg: { quality: 20 },
+});
+
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tsconfigPaths(), imagePlugin],
   appType: 'spa',
   base: '',
   build: {
